1d8cc80380f8648137f6d60bbc488bc000fdb6f2
AlexPoluha <145519519+AlexPoluha@users.noreply.github.com>
2024-10-29 12:41:40 +0500

    Update Program.cs

--- laba1/laba1/Program.cs
+++ laba1/laba1/Program.cs
@@ -1,21 +1,49 @@
-﻿using System;
+using System;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         Program p = new Program();
-        int x = 1211;
-        int a = 2;
-        int b = 0;
-        char c = 'H';
 
+        // Ввод данных с клавиатуры с проверками
+        int x = InputInt("Введите целое число x: ");
+        int a = InputInt("Введите целое число a: ");
+        int b = InputInt("Введите целое число b: ");
+        char c = InputChar("Введите символ c: ");
+
+        // Выполнение методов с выводом на экран
         Console.WriteLine("Сумма последних двух цифр: " + p.sum_two_last_nums(x));
         Console.WriteLine("Число положительно: " + p.positive_num(x));
         Console.WriteLine("Символ большой: " + p.capital_letter(c));
         Console.WriteLine("Делится нацело: " + p.is_divisor(a, b));
         Console.WriteLine("Сумма разрядов единиц: " + p.sum_last_num(a, b));
         Console.WriteLine("Деление: " + p.div(a, b));
+    }
+
+    public static int InputInt(string prompt)
+    {
+        int result;
+        while (true)
+        {
+            Console.Write(prompt);
+            if (int.TryParse(Console.ReadLine(), out result))
+                return result;
+            Console.WriteLine("Ошибка ввода! Пожалуйста, введите корректное целое число.");
+        }
+    }
+
+    public static char InputChar(string prompt)
+    {
+        char result;
+        while (true)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            if (!string.IsNullOrEmpty(input) && char.TryParse(input, out result))
+                return result;
+            Console.WriteLine("Ошибка ввода! Пожалуйста, введите один символ.");
+        }
     }
 
     // Задание 1
@@ -42,7 +70,6 @@ public class Program
     {
         return (a == 0 || b == 0) ? "Поделить невозможно" : (a % b == 0 || b % a == 0).ToString();
     }
-
 
     // 10
     public int sum_last_num(int a, int b)
